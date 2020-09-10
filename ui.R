46479526b8b0b7ae6aa392e330a808d328eef594
Kiko Siqueira <49378244+kikosikera@users.noreply.github.com>
2020-09-10 10:31:14 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,41 +1,33 @@
+##Peer-graded Assignment: Course Project: Shiny Application and Reproducible Pitch
+
+###Ammonia, the raw material for producing nitrogen fertilizer, is made from natural gas. The production cost of ammonia depends on the price of natural gas.
+###The production cost of ammonia($/tonne) is estimated as 36*Natural gas price($/mmbtu)+26
+###This shiny app calculates the production cost of ammonia as the change of natural gas price.
+
 library(shiny)
-library(markdown)
+
 shinyUI(fluidPage(
-    titlePanel("Investigation of Exponential Distribution"),
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("sliderLambda", "What is the lambda of the distribution?",
-                        0.1, 2, value = 0.2, step = 0.1),
-            sliderInput("sliderNsim", "How many simulations do you want to conduct?",
-                        500, 2000, value = 1000, step = 100),
-            sliderInput("sliderNmean", "How many exponentials do you want to take average from in each simulation?",
-                        5, 100, value = 40, step = 5),
-            numericInput("seed", "Input the seed for randomnization", 
-                         value = 1234, min = 1, max = 9999, step = 1),
-            
-            checkboxInput("showSampleMean", "Show/Hide Sample Mean", value = TRUE),
-            submitButton("Submit")
-        ),
-        mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Results",
-                                 h3("Exponential Distribution"),
-                                 plotOutput("plot1"),
-                                 h3("Distribution of the Mean"),
-                                 plotOutput("plot2"),
-                                 h3("Theoretical Mean:"),
-                                 textOutput("theoMean"),
-                                 h3("Sample Mean:"),
-                                 textOutput("sampleMean"),
-                                 h3("Theoretical Variance:"),
-                                 textOutput("theoVar"),
-                                 h3("Sample Variance:"),
-                                 textOutput("sampleVar")),
-                        tabPanel("Instructions",
-                                 mainPanel(
-                                     includeMarkdown("include.md")
-                                 ))
-            )
-        )
+
+  # Application title
+  titlePanel("Calculation of Ammonia/Urea production cost as the change of natural gas price"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("NG_price",
+                   "Natural gas price($/mmbtu)",
+                   min = 3,
+                   max = 16,
+                   value = 8,
+                   step=1),
+       checkboxInput("ammonia", "Production cost of Ammonia", value = TRUE),
+       checkboxInput("urea", "Production cost of Urea", value = TRUE)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("plot1"),
+       h3(textOutput("text"))
     )
+  )
 ))
