d54717872b4e63a323ad4b0d56405c57420e3892
Kiko Siqueira <49378244+kikosikera@users.noreply.github.com>
2020-09-10 10:32:33 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,30 +4,30 @@
 ###The production cost of ammonia($/tonne) is estimated as 36*Natural gas price($/mmbtu)+26
 ###This shiny app calculates the production cost of ammonia as the change of natural gas price.
 
+
 library(shiny)
 
-shinyUI(fluidPage(
+shinyServer(function(input, output) {
 
-  # Application title
-  titlePanel("Calculation of Ammonia/Urea production cost as the change of natural gas price"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("NG_price",
-                   "Natural gas price($/mmbtu)",
-                   min = 3,
-                   max = 16,
-                   value = 8,
-                   step=1),
-       checkboxInput("ammonia", "Production cost of Ammonia", value = TRUE),
-       checkboxInput("urea", "Production cost of Urea", value = TRUE)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("plot1"),
-       h3(textOutput("text"))
-    )
-  )
-))
+  output$plot1 <- renderPlot({
+          x = c(3:16)
+          y = 36*x+26
+          plot(x,y,xlab="Natural gas price($/mmbtu)", ylab="Production cost($/tonne)",col="white")
+          legend("topleft",legend=c("Production cost of Ammonia", "Production cost of Urea"), col=c("red","blue"),lty=1)
+          if(input$ammonia){
+                  x = c(3:16)
+                  y = 36*x+26
+                  z = 26*x+42
+                  NG=input$NG_price
+                  lines(x,y, col = "red", lwd= 2)
+                  points(NG, 36*NG+26, col = "red", pch = 16, cex = 2)}
+          if(input$urea){
+                  x = c(3:16)
+                  y = 36*x+26
+                  z = 26*x+42
+                  NG=input$NG_price
+          lines(x,z, col = "blue",lwd=2)
+          points(NG, 26*NG+42, col = "blue", pch = 16, cex = 2)}
+  })
+  output$text <- renderText("Ammonia and Urea are both raw materials for producing nitrogen fertilizer. They are made from natural gas. The production cost of ammonia and Urea depends on the price of natural gas. This shiny app calculates the production cost of ammonia/Urea as the change of natural gas price.")
+})
